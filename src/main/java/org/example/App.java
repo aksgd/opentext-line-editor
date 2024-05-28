38b3754fb98ccd20754a72eba9d20b674b5d799d
aksgd <ak.ksgd@gmail.com>
2024-05-28 11:17:29 +0530

    read and display file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws FileNotFoundException {
+        var file = new File(args[0]);
+        var br
+            = new BufferedReader(new FileReader(file));
+        var linesOfFile = new LinkedList<String>();
+        br.lines().forEach(line -> {
+            linesOfFile.add(line);
+        });
+        linesOfFile.forEach(line -> {
+            System.out.println(line);
+        });
+
     }
 }
