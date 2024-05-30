427881c9f09ee0ad6380ba7e76c7fd5a61ec2f50
aksgd <ak.ksgd@gmail.com>
2024-05-30 10:17:00 +0530

    test cases for input parsing

--- src/test/java/com/opentext/assignment/linereader/InputTest.java
+++ src/test/java/com/opentext/assignment/linereader/InputTest.java
@@ -5,16 +5,34 @@ import com.opentext.assignment.linereader.Input;
 
 public class InputTest extends TestCase {
 
-    public void testInsert() {
+    public void testInsert_happypath() {
         var input = new Input("ins 1");
         assertEquals(input.getFlag(), "ins");
         assertEquals(input.getValueAsInteger(), Integer.valueOf(1));
     }
 
-    public void testDelete() {
-        var input = new Input("del 1");
+    public void testInsert_no_line_number() {
+        var input = new Input("ins");
+        assertEquals(input.getFlag(), "ins");
+        assertEquals(input.getValueAsInteger(), null);
+    }
+
+    public void testDelete_happypath_no_line_number() {
+        var input = new Input("del");
         assertEquals(input.getFlag(), "del");
-        assertEquals(input.getValueAsInteger(), Integer.valueOf(1));
+        assertEquals(input.getValueAsInteger(), null);
+    }
+
+    public void testInsert_invalid_line_number() {
+        var input = new Input("ins xx");
+        assertEquals(input.getFlag(), "ins");
+        assertEquals(input.getValueAsInteger(), null);
+    }
+
+    public void testDelete_invalid_line_number() {
+        var input = new Input("del xx");
+        assertEquals(input.getFlag(), "del");
+        assertEquals(input.getValueAsInteger(), null);
     }
 
 }
